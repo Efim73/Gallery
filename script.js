1b181a59378c4eb479b5ded8fd1b06554c2bfb3a
Efim73 <82439026+Efim73@users.noreply.github.com>
2022-02-08 11:52:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 let fullSize = document.getElementById('fullSize')
-function openImage(){
+let fullScreen = document.getElementById('fullScreen')
+let rightA  = document.getElementById('rightA')
+let leftA = document.getElementById('leftA')
+let images = [];
+let currentImage;
+for(let i = 1; i<10; i++){
+    images.push('img'+i+'.jpg');
+
+}
+function openImage(imgNumber){
+    fullSize.style.transition = '0.5s'
+    currentImage = imgNumber;
+    fullSize.children[0].src='img'+imgNumber+'.jpg';
     fullSize.style.transform='translateY(0%)'
+
     
+}
+rightA.onclick = function(){
+    console.log(images);
+    currentImage = currentImage+1
+    if(currentImage>images.length){
+        currentImage=1;
+    }
+    fullScreen.src = 'img'+currentImage+'.jpg'
+    
+
+}
+leftA.onclick = function(){
+    currentImage = currentImage-1
+    if(currentImage<1){
+        currentImage = images.length;
+    }
+    fullScreen.src = 'img'+currentImage+'.jpg'
+}
+fullScreen.onclick = function(){
+    fullSize.style.transform='translateY(-100%)'
 }
